0519161a0f99367cd03815bff676259ee22a69af
bhaskar966 <deybhaskar999@gmail.com>
2025-09-04 15:07:11 +0530

    Add Privacy Policy and Terms & Conditions page for Bee Walls
    Also install react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import PrivacyPolicy from './beewalls/PrivacyPolicy.jsx'
+import './index.css'
+import TermsAndConditions from './beewalls/TermsAndConditions.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/beewalls/privacyPolicy',
+    element: <PrivacyPolicy />,
+  },
+  {
+    path: '/beewalls/termsAndConditions',
+    element: <TermsAndConditions />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
